e26e0327a8baee135b92517e009ee3f7b72a3e16
sjg <sjg@sjgdeMacBook-Pro.local>
2016-07-16 00:51:25 +0800

    sodas

--- TomCat/ViewController.m
+++ TomCat/ViewController.m
@@ -9,7 +9,11 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+{
+    NSDictionary *_dataDic;
+    
+}
+@property (weak, nonatomic) IBOutlet UIImageView *bgview;
 @end
 
 @implementation ViewController
@@ -17,6 +21,38 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"Tomcat" ofType:@"plist"];
+    _dataDic = [NSDictionary dictionaryWithContentsOfFile:path];
+}
+- (IBAction)btnClick:(UIButton *)sender {
+    NSDictionary *subDic = nil;
+    switch (sender.tag) {
+        case 1:
+        {
+            subDic = [_dataDic objectForKey:@"cymbal"];
+        }
+            break;
+            
+            // aaaa
+            
+        case 2:
+        {
+            subDic = [_dataDic objectForKey:@"fart"];
+        }
+            break;
+            
+        default:
+            break;
+    }
+    NSMutableArray *imageArr = [NSMutableArray array];
+    for (int i=0; i<[[subDic objectForKey:@"frames"] intValue]; i++) {
+        [imageArr addObject:[UIImage imageNamed:[NSString stringWithFormat:[subDic objectForKey:@"imageFormat"],i]]];
+        
+    }
+    _bgview.animationImages = imageArr;
+    _bgview.animationDuration = imageArr.count/10;
+    _bgview.animationRepeatCount = 1;
+    [_bgview startAnimating];
 }
 
 - (void)didReceiveMemoryWarning {
